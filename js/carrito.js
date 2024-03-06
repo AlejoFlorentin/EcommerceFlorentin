1403cf10618b578e264e323ca7e3d902cd4ad233
Alejo Florentin <alejoflorentin@gmail.com>
2024-03-06 15:32:12 -0300

    Agregando librerias y comentando codigo

--- js/carrito.js
+++ js/carrito.js
@@ -1,3 +1,4 @@
+//6)Llamamos a la local storage donde estan los productos q agregamos en la seccion productos y tambien seleccionamos los elementos HTML que utilizaremos
 let productosCarrito=localStorage.getItem("prendasCarrito")
 productosCarrito=JSON.parse(productosCarrito)
 
@@ -9,23 +10,24 @@ let btnsEliminar=document.querySelectorAll(".carritoProductoEliminar")
 const botonVaciar = document.querySelector("#vaciarCarrito")
 const botonComprar =document.querySelector("#comprarCarrito")
 
+//7)Creamos una funcion que cargue los productos del carrito
 function cargarProductosCarrito(){
 
-if (productosCarrito && productosCarrito.length >0) {
+if (productosCarrito && productosCarrito.length >0) { // Si hay productos en el carrito se hace lo de dentro del IF y sino lo del ELSE
 
     
 
-    carritoVacio.classList.add("disabled")
-    carritoProductos.classList.remove("disabled")
+    carritoVacio.classList.add("disabled") 
+    carritoProductos.classList.remove("disabled")       //a)Descativamos y activamos los estilos css que necesitemos si hay productos en el carrito
     carritoAcciones.classList.remove("disabled")
     carritoComprado.classList.add("disabled")
  
-    carritoProductos.innerHTML=""
+    carritoProductos.innerHTML=""  //b) Inserto este string vacio asi cada vez q entramos al carrito arranco de cero y continua el codigo de abajo y no se repiten los productos
 
-    productosCarrito.forEach(prenda=>{
+    productosCarrito.forEach(prenda=>{ //c)Utilizamos el metodo forEach que nos devuelve todos los elementos del carrito
 
-    const div= document.createElement("div")
-    div.classList.add("carritoProducto")
+    const div= document.createElement("div") //d) Creamos un elemento y lo almacenamos en una variable
+    div.classList.add("carritoProducto") //e) A este elemento le agregamos la clase "carritoProducto" , la cual tiene ciertos estilos de css
     div.innerHTML=`<img src="${prenda.imagen}" alt="${prenda.nombre}">
                     <div class="carritoProductoTitulo">
                         <small>Titulo</small>
@@ -33,7 +35,7 @@ if (productosCarrito && productosCarrito.length >0) {
                     </div>
                     <div class="carritoProductoCantidad">
                         <small>Cantidad</small>
-                        <p>${prenda.cantidad}</p>
+                        <p>${prenda.cantidad}</p>               
                     </div>
                     <div class="carritoProductoPrecio">
                         <small>Precio</small>
@@ -44,9 +46,9 @@ if (productosCarrito && productosCarrito.length >0) {
                         <p>$ ${prenda.precio*prenda.cantidad}</p>
                     </div>
                     <button class="carritoProductoEliminar" id="${prenda.id}"><i class='bx bxs-trash'></i></button>
-                   ` 
+                   ` //f) Al elemento creado le introducimos este codigo html con las propiedades de los objetos del CARRITO que recorrimos con el metodo forEach , para eso sirvio ese metodo
 
-    carritoProductos.append(div)
+    carritoProductos.append(div) //g)Con .append agregamos todo lo anterior al contenedor seleccionador anteriormente
     })
 
     actualizarBotonesEliminar()
@@ -55,7 +57,7 @@ if (productosCarrito && productosCarrito.length >0) {
 }else{
 
     carritoVacio.classList.remove("disabled")
-    carritoProductos.classList.add("disabled")
+    carritoProductos.classList.add("disabled")  //h)Se vuelven a activar los estilos css como estaban al inicio de la funcion si es que no hay productos
     carritoAcciones.classList.add("disabled")
     carritoComprado.classList.add("disabled")
 
@@ -63,52 +65,98 @@ if (productosCarrito && productosCarrito.length >0) {
 
 }
 
-cargarProductosCarrito()
 
+cargarProductosCarrito() //i)Llamamos a la funcion para que nos muestre los productos del carrito
+//8)Creamos una funcion con un EVENTO para q cuando hagamos CLICK en el boton eliminar nos elimine una prenda del carrito
 function actualizarBotonesEliminar() {
-    btnsEliminar = document.querySelectorAll(".carritoProductoEliminar");
-
-    btnsEliminar.forEach(boton => {
-        boton.addEventListener("click", eliminarDelCarrito);
-    });
+    btnsEliminar = document.querySelectorAll(".carritoProductoEliminar"); // a) La variable donde llamamos a todos los btn eliminar fue creada con LET por que siempre
+                                                                            // cambian la cantidad de botones , si la hiciesemos con CONST daria error ya que 
+                                                                            // ese tipo de variable no se puede cambiar de valor
+    btnsEliminar.forEach(boton => { // b)Aplicamos forEach para buscar todos los btns eliminar y meterlos en un array
+        boton.addEventListener("click", eliminarDelCarrito);// c) Aca aplicamos evento click a cada boton que aparece
+    }); 
+    //d)Lo agregamos al final del IF de la funcion cargarProductosEnCarrito asi cada vez que se agregan productos se actualizar el nro de botones eliminar
 }
-
+//9)Creamos la funcion que eliminar prendas del carrito 
 function eliminarDelCarrito(e){
 
-    const idBoton = e.currentTarget.id;
-    const index = productosCarrito.findIndex(producto => producto.id === idBoton);
-    
-    productosCarrito.splice(index, 1);
-    cargarProductosCarrito();
+ Toastify({
 
-    localStorage.setItem("prendasCarrito", JSON.stringify(productosCarrito));
+    text: "Producto Eliminado",
+    duration: 3000,
+    newWindow: true,
+    offset: {
+    x: "2rem", // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+    y: 2 // vertical axis - can be a number or a string indicating unity. eg: '2em'
+  },
+    gravity: "top", // `top` or `bottom`
+    position: "right", // `left`, `center` or `right`
+     stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+    background: "black",
+    borderRadius:"2rem"
+    },
+  onClick: function(){} // Callback after click
+            
+}).showToast();
+
+    const idBoton = e.currentTarget.id;//a)Almacenamos el id del boton aliminar clickeado
+    const index = productosCarrito.findIndex(producto => producto.id === idBoton); //b)buscamos el idBoton que coincida con algun producto del carrito y almacenamos
+                                                                                    // con .findIndex el indice  del elemento del carrito que coincidio
+    
+    productosCarrito.splice(index, 1);//c)Aplicamos el metodo .splice donde le pasamos como param el INDEX del producto que queremos eliminar y como segundo parametro la cantidad
+    cargarProductosCarrito(); //d)Volvemos a cargar los productos del carrito
+
+    localStorage.setItem("prendasCarrito", JSON.stringify(productosCarrito));//e)Volvemos a guardar los productos del carrito en la localStorage ya creada sin los productos eliminados
 
 }
 
-botonVaciar.addEventListener("click", vaciarCarrito)
-
+//10)Hacemos funcional el boton vaciar carrito
+botonVaciar.addEventListener("click", vaciarCarrito) //a)aplicamos un evento al clickearlo y le pasamos una funcion
+//b)creamos la funcion q vacie el carrito al hacer click
 function vaciarCarrito(){
 
-    productosCarrito.length = 0;
-    localStorage.setItem("prendasCarrito", JSON.stringify(productosCarrito));
-        cargarProductosCarrito();
+    Swal.fire({
 
+        title: "¿Estas seguro?",
+        icon: "question",
+        html: `Se vaciara el carrito`,
+        showCancelButton: true,
+        focusConfirm: false,
+        confirmButtonText: `Si`,
+        cancelButtonText: `No`,
+
+    }).then((result) => {
+  
+        if (result.isConfirmed) {
+             productosCarrito.length = 0; //c) con .length modificamos la cant de elementos del carrito y lo llevamos a 0 , de esta forma ya no habra productos en el carrito
+            localStorage.setItem("prendasCarrito", JSON.stringify(productosCarrito)); //d) volvemos a guardar esto en la localStorage ya creada y ahora el carrito estara vacio
+            cargarProductosCarrito(); //e)cargamos los productos en el carrito que serian 0
+
+        } 
+    });
+
+   
 }
 
+//11)Creamos una funcion q actualize el titulo de total
 function actualizarTotal() {
-    const totalCalculado = productosCarrito.reduce((acc, producto) => acc + (producto.precio * producto.cantidad), 0);
-    total.innerText = `$${totalCalculado}`;
+    const totalCalculado = productosCarrito.reduce((acc, producto) => acc + (producto.precio * producto.cantidad), 0);//a)con .reduce acumulamos los precios de todos los productos y los sumamos
+                                                                                                                        //almacenados en una variable
+    total.innerText = `$${totalCalculado}`; //b)luego con innerText se lo agregamos al elemento
 }
+//d)Lo agregamos al final del IF de la funcion cargarProductosEnCarrito asi cada vez que se agregan productos se actualiza el total
 
-botonComprar.addEventListener("click", comprarCarrito);
-function comprarCarrito() {
+//12)Hacemos funcional el boton comprar
+botonComprar.addEventListener("click", comprarCarrito);//a)aplicamos un evento al clickearlo y le pasamos una funcion
+function comprarCarrito() { //b)creamos la funcion q vacie el carrito al hacer click
 
-    productosCarrito.length = 0;
-    localStorage.setItem("prendasCarrito", JSON.stringify(productosCarrito));
+    productosCarrito.length = 0;//c) con .length modificamos la cant de elementos del carrito y lo llevamos a 0 , de esta forma ya no habra productos en el carrito
+    localStorage.setItem("prendasCarrito", JSON.stringify(productosCarrito));//d) volvemos a guardar esto en la localStorage ya creada y ahora el carrito estara vacio Y "COMPRADO"
     
     carritoVacio.classList.add("disabled");
-    carritoProductos.classList.add("disabled");
-    carritoAcciones.classList.add("disabled");
+    carritoProductos.classList.add("disabled");  //e)Volvemos a activar y descactivar los estilos css necesarios
+    carritoAcciones.classList.add("disabled");  
     carritoComprado.classList.remove("disabled");
 
 }
--- js/productos.js
+++ js/productos.js
@@ -1,168 +1,13 @@
-const prendas = [
+let prendas=[]
 
-{
-    id:"1",
-    nombre:"Remera Oversize Negra",
-    precio:25000,
-    categoria:"Remeras",
-    imagen:"/Assets/img/remeras/remeraOvNegra.png",
+fetch("/js/prendas.json")
+    .then(response => response.json())
+    .then(data => {
+        prendas = data
+        mostrarProductos(prendas)
+    })
 
-},
-{
-    id:"2",
-    nombre:"Remera Oversize Roja",
-    precio:25000,
-    categoria:"Remeras",
-    imagen:"/Assets/img/remeras/remeraOvRoja.jpg",
-
-},
-{
-    id:"3",
-    nombre:"Remera Oversize Beige",
-    precio:25000,
-    categoria:"Remeras",
-    imagen:"/Assets/img/remeras/remeraOvBeige.webp",
-
-},
-{
-    id:"4",
-    nombre:"Remera Oversize Estampada",
-    precio:30000,
-    categoria:"Remeras",
-    imagen:"/Assets/img/remeras/remeraOvEstampa.jpg",
-
-},
-{
-    id:"5",
-    nombre:"Jean Roto Recto",
-    precio:60000,
-    categoria:"Pantalones",
-    imagen:"/Assets/img/pantalones/jeanRotoRecto.webp",
-
-},
-{
-    id:"6",
-    nombre:"Jogger Negro",
-    precio:45000,
-    categoria:"Pantalones",
-    imagen:"/Assets/img/pantalones/joggerNegro.webp",
-
-},
-{
-    id:"7",
-    nombre:"Jean Recto Negro",
-    precio:50000,
-    categoria:"Pantalones",
-    imagen:"/Assets/img/pantalones/jeanNegroRecto.webp",
-
-},
-{
-    id:"8",
-    nombre:"Pantalon Cargo",
-    precio:47000,
-    categoria:"Pantalones",
-    imagen:"/Assets/img/pantalones/pantalonCargo.jpg",
-
-},
-{
-    id:"9",
-    nombre:"Bermuda Cargo",
-    precio:40000,
-    categoria:"Bermudas",
-    imagen:"/Assets/img/bermudas/bermudaCargo.webp",
-
-},
-{
-    id:"10",
-    nombre:"Bermuda de Jean Rota",
-    precio:30000,
-    categoria:"Bermudas",
-    imagen:"/Assets/img/bermudas/bermudaDeJeanRota.webp",
-
-},
-{
-    id:"11",
-    nombre:"Bermuda Negra",
-    precio:40000,
-    categoria:"Bermudas",
-    imagen:"/Assets/img/bermudas/bermudaNegra.jpg",
-
-},
-{
-    id:"12",
-    nombre:"Bermuda de jean",
-    precio:45000,
-    categoria:"Bermudas",
-    imagen:"/Assets/img/bermudas/bermudaJean.webp",
-
-},
-{
-    id:"13",
-    nombre:"Nike Air Force 07 White",
-    precio:180000,
-    categoria:"Zapatillas",
-    imagen:"/Assets/img/zapatillas/AirForce07.png",
-
-},
-{
-    id:"14",
-    nombre:"Adidas Yeezy 350",
-    precio:250000,
-    categoria:"Zapatillas",
-    imagen:"/Assets/img/zapatillas/Yeezy 350.jpg",
-
-},
-{
-    id:"15",
-    nombre:"Nike Dunk Low Panda",
-    precio:220000,
-    categoria:"Zapatillas",
-    imagen:"/Assets/img/zapatillas/dunkPanda.jpg",
-
-},
-{
-    id:"16",
-    nombre:"Nike Blazer Mid",
-    precio:150000,
-    categoria:"Zapatillas",
-    imagen:"/Assets/img/zapatillas/blazerMid.jpg",
-
-},
-{
-    id:"17",
-    nombre:"Boxer Negro",
-    precio:7000,
-    categoria:"Accesorios",
-    imagen:"/Assets/img/accesorios/boxer negro.jpg",
-
-},
-{
-    id:"18",
-    nombre:"Boxer Azul Marino",
-    precio:7000,
-    categoria:"Accesorios",
-    imagen:"/Assets/img/accesorios/boxerAzulMarino.webp",
-
-},
-{
-    id:"19",
-    nombre:"Piluso Supreme",
-    precio:15000,
-    categoria:"Accesorios",
-    imagen:"/Assets/img/accesorios/pilusoSupreme.jpg",
-
-},
-{
-    id:"20",
-    nombre:"Medias Supreme",
-    precio:10000,
-    categoria:"Accesorios",
-    imagen:"/Assets/img/accesorios/mediasSupreme.jpg",
-
-},
-
-
-]
+//Selecciono y almaceno en variables los elementos html de productos.html que voy a utilizar para aplicarle JS
 
 const contenedorPrendas=document.querySelector("#contenedorProductos")
 const btnsCategorias=document.querySelectorAll(".botonCategoria")
@@ -170,15 +15,17 @@ const tituloPrincipal=document.querySelector("#tituloPrincipal")
 let btnAgregar=document.querySelectorAll(".prendaAgregar")
 const numerito=document.querySelector("#numerito")
 
+// 1°) Creo una funcion que muestre  las prendas que quiero del array
 
 function mostrarProductos(productosSeleccionados) {
 
-    contenedorPrendas.innerHTML=""
+    contenedorPrendas.innerHTML="" //a) Inserto este string vacio asi cada vez q entramos a una seccion arranco de cero y continua el codigo de abajo y no se repiten los productos
 
+    //b)Utilizamos el metodo forEach que nos devuelve todos los elementos del array de objetos
     productosSeleccionados.forEach(prenda=>{
 
-    const div=document.createElement("article")
-    div.classList.add("producto")
+    const div=document.createElement("article") //c) Creamos un elemento y lo almacenamos en una variable
+    div.classList.add("producto") //d) A este elemento le agregamos la clase "producto" , la cual tiene ciertos estilos de css
     div.innerHTML=`
         <img class="productoImagen" src="${prenda.imagen}" alt="${prenda.nombre}">
         <div class="productoInfo">
@@ -186,32 +33,40 @@ function mostrarProductos(productosSeleccionados) {
             <p class="productoPrecio">$ ${prenda.precio}</p>
             <button class="prendaAgregar" id="${prenda.id}">Agregar</button>
         </div>
-        `
-    contenedorPrendas.append(div)    
+        `//e) Al elemento creado le introducimos este codigo html con las propiedades de los objetos del array que recorrimos con el metodo forEach , para eso sirvio ese metodo
+    contenedorPrendas.append(div)
+    //f)Con .append agregamos todo lo anterior al contenedor seleccionador anteriormente    
     })
 
     actualizarBtnsAgregar()
 
 }
 
-mostrarProductos(prendas)
 
+
+// 2) Aplico un "EVENTO" a los todos botones de las distintas "CATEGORIAS" para que cuando les haga "CLICK" me "FILTRE" las prendas que corresponden
+// a dicho boton
+
+//a) Aplico un forEach a la variable btnsCategorias que me va a devolver un array con todos los botones
 btnsCategorias.forEach(btn =>{
 
-    btn.addEventListener("click",e=>{
+    btn.addEventListener("click",e=>{ //b) A cada boton le aplico un EVENTO CLICK 
 
-        btnsCategorias.forEach(btn=>btn.classList.remove("active"))
-        e.currentTarget.classList.add("active")
+        btnsCategorias.forEach(btn=>btn.classList.remove("active"))//c) Al hacerle click vuelve a recorrer los btns y se asegura de removerle la clase active a todos
+        e.currentTarget.classList.add("active") //d)Luego de removerle la clase active a todos , se la activa al boton que clickeamos (el currentTarget se utiliza para 
+                                                                                                                                        // para que si o si apuntemos al boton)
+        if(e.currentTarget.id != "todos"){ 
 
-        if(e.currentTarget.id != "todos"){
+        const prendaCategoria=prendas.find(producto=>producto.categoria===e.currentTarget.id)   // e) Si el id del btn es distinto que el id="todos" buscamos con el metodo find
+        tituloPrincipal.innerText=prendaCategoria.categoria                                     // en el array de objetos la primerr prendaa q coincida ,la categoria y el id del boton y tambien
+                                                                                                // cambiamos con innerText el elemento titulo principal por la categoria elegida
+                                                                                                
 
-        const prendaCategoria=prendas.find(producto=>producto.categoria===e.currentTarget.id)
-        tituloPrincipal.innerText=prendaCategoria.categoria
+        const prodBtn=prendas.filter(prenda=>prenda.categoria==e.currentTarget.id) // f) Con el metodo .filter buscamos todos los elementos en la que su categoria coincidan con el id
+        mostrarProductos(prodBtn)                                                   // del boton y llamamos a la funcion mostrarProductos para mostrarlos
+        
 
-        const prodBtn=prendas.filter(prenda=>prenda.categoria==e.currentTarget.id)
-        mostrarProductos(prodBtn)
-
-        }else{
+        }else{ // g) Sino pasa lo anterior mostramos todos los productos
 
         tituloPrincipal.innerText="Todos los productos"    
         mostrarProductos(prendas)
@@ -222,67 +77,98 @@ btnsCategorias.forEach(btn =>{
 
 })
 
+
+//3)Creamos una funcion con un EVENTO para q cuando hagamos CLICK en el boton agregar nos agregue una prenda al carrito
 function actualizarBtnsAgregar(){
 
-  btnAgregar=document.querySelectorAll(".prendaAgregar")  
+  btnAgregar=document.querySelectorAll(".prendaAgregar")  // a) La variable donde llamamos a todos los btn Agregar fue creada con LET por que siempre
+                                                            // cambian la cantidad de botones , si la hiciesemos con CONST daria error ya que 
+                                                            // ese tipo de variable no se puede cambiar de valor
 
-  btnAgregar.forEach(boton=>{
+  btnAgregar.forEach(boton=>{ // Aplicamos forEach para buscar todos los btns Agregar y meterlos en un array
 
-    boton.addEventListener("click",agregarAlCarrito)
+    boton.addEventListener("click",agregarAlCarrito) // b) Aca aplicamos evento click a cada boton que aparece
 
   })
 
-}
+} //e La funcion la agregamos al fondo de la funcion mostrarProductos ya que al cambiar la cant de productos cuando filtramos los productos por su categoria cambian
+ //la cant de btns agregar
 
 
-
+//4) Creacion carrito
 let carrito
 
-let productosCarritoLS=localStorage.getItem("prendasCarrito")
+let productosCarritoLS=localStorage.getItem("prendasCarrito") //i)llamamos a la local storage con los productos guardados sin parsearlos
 
 if (productosCarritoLS) {
 
-    carrito=JSON.parse(productosCarritoLS)
+    carrito=JSON.parse(productosCarritoLS) //j)Si hay productos guardado  parsearlos y guardarlos en carrito y luego actualizar el numerito
     actualizarNumerito()
 
 } else {
 
-    carrito=[]
+    carrito=[] //k)Sino dejarlo vacio y no actualizar
 
 }
 
 
-
+//a)Creamos la funcion que agrega prendas al carrito donde si el idBtn no se encuentra pasa al ELSE y se agrega el producto al carrito y si se repite
+// pasa al if y se aumenta el numero de cantidad
 function agregarAlCarrito(e){
 
-    const idBtn=e.currentTarget.id
-    const prendaAgregada=prendas.find(prnda => prnda.id === idBtn)
-    
-    if (carrito.some(prenda=>prenda.id===idBtn)) {
+    Toastify({
 
-        const index=carrito.findIndex(prenda => prenda.id === idBtn)
-        carrito[index].cantidad++
+    text: "Producto Agregado",
+    duration: 3000,
+    destination: "http://127.0.0.1:5500/pages/carrito.html",
+    newWindow: true,
+    offset: {
+    x: "2rem", 
+    y: 2 
+  },
+    gravity: "top", 
+    position: "right", 
+     stopOnFocus: true, 
+    style: {
+    background: "black",
+    borderRadius:"2rem"
+    },
+  onClick: function(){} 
+            
+}).showToast();
+
+    const idBtn=e.currentTarget.id //b)Guardamos el id del btnAgregar clickeado en una variable
+    const prendaAgregada=prendas.find(prnda => prnda.id === idBtn) //c)Le aplicamos el metodo .find al array de productos para q nos traiga el primer producto
+                                                                    // q coincida con el id guardado anteriormente
+    if (carrito.some(prenda=>prenda.id===idBtn)) { //e)Aplicamos el metodo .some para ver si la prenda existe en el carrito 
+
+        const index=carrito.findIndex(prenda => prenda.id === idBtn)//g)Si la prenda existe buscamos el index en el carrito q coincida con el id del btn agregar para luego
+        carrito[index].cantidad++                                   // aumentar su cantidad
         
         
     }else{
      
      prendaAgregada.cantidad=1   
-     carrito.push(prendaAgregada) 
+     carrito.push(prendaAgregada) //f)Si la prenda no esta en el carrito  se lo coloca una propiedad nueva llamada cantidad con 
+                                    // un valor de 1 y se agrega al carrito , esto se hace para luego si se agrega de vuelta aumentar la propiedad cantidad y no el nro de elementos del array carrito
 
     }
     
     actualizarNumerito()
 
-    localStorage.setItem("prendasCarrito",JSON.stringify(carrito))
+    localStorage.setItem("prendasCarrito",JSON.stringify(carrito))//h)Guardamos todos los productos agregados en la localStorage
 
 }
 
-
+//5)Creamos la funcion actualizar numerito de la cantidad de articulos en el carrito
 function actualizarNumerito(){
 
-let nuevoNumerito = carrito.reduce((acc,prenda) => acc + prenda.cantidad , 0 )
-numerito.innerText=nuevoNumerito
+let nuevoNumerito = carrito.reduce((acc,prenda) => acc + prenda.cantidad , 0 )//a)A la variable carrito le aplicamos el metodo reduce para acumular la cant de prendas agregadas
+                                                                                // y sumarlas y la almacenamos en la variable LET nuevo numerito , usamos ese tipo de variable
+                                                                                //ya que este numero cambiara siempre
+numerito.innerText=nuevoNumerito //b)Luego con inner text agregamos el valor de nuevoNumerito en el elemento numerito del html
 }
+//c)La funcion la agregaremos abajo de todo de la funcion agregarAlcarrito asi el numero se va actualizando a medida que se vayan agregando productos
 
 
 
